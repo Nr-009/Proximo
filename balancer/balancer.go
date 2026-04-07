23410539219b95fcf491fdf492b7e8292943a2cc
Nr-009 <n.rubiano01@gmail.com>
2026-04-07 11:59:43 -0500

    completed the power of two routing

--- balancer/balancer.go
+++ balancer/balancer.go
@@ -13,14 +13,16 @@ type Balancer interface {
 }
 
 func New(strategy string, servers *[]*backends.Server) (Balancer, error) {
-    switch strategy {
-    case "round-robin":
-        return NewRoundRobin(servers), nil
-    case "weighted":
-        return NewWeighted(servers), nil
-    case "least-conn":
-        return NewLeastConn(servers), nil
-    default:
-        return nil, errors.New("unknown strategy: " + strategy)
-    }
+	switch strategy {
+	case "round-robin":
+		return NewRoundRobin(servers), nil
+	case "weighted":
+		return NewWeighted(servers), nil
+	case "least-conn":
+		return NewLeastConn(servers), nil
+	case "p2c":
+		return NewPowerOfTwo(servers), nil
+	default:
+		return nil, errors.New("unknown strategy: " + strategy)
+	}
 }
--- balancer/poweroftwo.go
+++ balancer/poweroftwo.go
@@ -1,1 +1,63 @@
 package balancer
+
+import (
+	"errors"
+	"log"
+	"math/rand"
+	"sync/atomic"
+
+	"github.com/Nr-009/Proximo/backends"
+)
+
+type PowerOfTwo struct {
+	servers *[]*backends.Server
+}
+
+func NewPowerOfTwo(servers *[]*backends.Server) *PowerOfTwo {
+	return &PowerOfTwo{servers: servers}
+}
+
+func (p *PowerOfTwo) NextServer() (*backends.Server, error) {
+	active := make([]*backends.Server, 0)
+	for _, s := range *p.servers {
+		if s.Active {
+			active = append(active, s)
+		}
+	}
+
+	if len(active) == 0 {
+		return nil, errors.New("all servers are inactive")
+	}
+
+	if len(active) == 1 {
+		atomic.AddInt64(&active[0].Connections, 1)
+		return active[0], nil
+	}
+
+	i := rand.Intn(len(active))
+	j := rand.Intn(len(active) - 1)
+	if j >= i {
+		j++
+	}
+
+	a := active[i]
+	b := active[j]
+
+	var best *backends.Server
+	if atomic.LoadInt64(&a.Connections) <= atomic.LoadInt64(&b.Connections) {
+		best = a
+	} else {
+		best = b
+	}
+
+	atomic.AddInt64(&best.Connections, 1)
+	return best, nil
+}
+
+func (p *PowerOfTwo) OnAddServer(s *backends.Server) {
+	log.Printf("[p2c] notified of server on port %d", s.Port)
+}
+
+func (p *PowerOfTwo) OnRemoveServer(s *backends.Server) {
+	log.Printf("[p2c] notified of removal on port %d", s.Port)
+}
